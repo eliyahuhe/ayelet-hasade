f7f2390bb8ffb151c5a6b14e87d9ec9ad0bce54d
eliyahuhe <eliheskiel@gmail.com>
2026-05-01 12:50:18 +0300

    להתחיל לעבוד על ה JS + - כפתור

---
+++ JS/data.js
@@ -0,0 +1,28 @@
+
+const products = [
+
+  { id: 1, name: "גזר", price: 5.9, unit: "ק״ג", image: "image/vegetables/carrot.png", category: "vegetables", sale: true, stock: 18 },
+  { id: 2, name: "מלפפון", price: 6.9, unit: "ק״ג", image: "image/vegetables/cucumber.png", category: "vegetables", stock: 12 },
+  { id: 3, name: "חציל", price: 5, unit: "ק״ג", image: "image/vegetables/eggplant.png", category: "vegetables", stock: 7 },
+  { id: 4, name: "שום", price: 15, unit: "ק״ג", image: "image/vegetables/garlic.png", category: "vegetables", stock: 5 },
+  { id: 5, name: "בצל סגול", price: 6, unit: "ק״ג", image: "image/vegetables/PurpleOnion.png", category: "vegetables", stock: 14 },
+  { id: 6, name: "גמבה אדומה", price: 9, unit: "ק״ג", image: "image/vegetables/RedPepper.png", category: "vegetables", stock: 9 },
+  { id: 7, name: "בטטה", price: 7, unit: "ק״ג", image: "image/vegetables/sweetPotato.png", category: "vegetables", stock: 11 },
+  { id: 8, name: "עגבניה", price: 8, unit: "ק״ג", image: "image/vegetables/Tomatoes.png", category: "vegetables", stock: 20 },
+  { id: 9, name: "בצל לבן", price: 5, unit: "ק״ג", image: "image/vegetables/whiteOnion.png", category: "vegetables", stock: 13 },
+  { id: 10, name: "תפוח אדמה", price: 4, unit: "ק״ג", image: "image/vegetables/WhitePotato.jpg", category: "vegetables", stock: 16 },
+
+  { id: 11, name: "תפוח", price: 10, unit: "ק״ג", image: "image/fruits/Apple.png", category: "fruits", sale: true, stock: 10 },
+  { id: 12, name: "בננה", price: 6, unit: "ק״ג", image: "image/fruits/banana.png", category: "fruits", stock: 17 },
+  { id: 13, name: "קוקוס", price: 12, unit: "ק״ג", image: "image/fruits/coconut.png", category: "fruits", stock: 6 },
+  { id: 14, name: "ענבים", price: 20, unit: "ק״ג", image: "image/fruits/grapes.png", category: "fruits", stock: 8 },
+  { id: 15, name: "קיווי", price: 12, unit: "ק״ג", image: "image/fruits/Kiwi.jpg", category: "fruits", stock: 15 },
+  { id: 16, name: "מנגו", price: 15, unit: "ק״ג", image: "image/fruits/mango.png", category: "fruits", stock: 9 },
+  { id: 17, name: "תפוז", price: 7, unit: "ק״ג", image: "image/fruits/orange.png", category: "fruits", stock: 19 },
+  { id: 18, name: "אננס", price: 25, unit: "ק״ג", image: "image/fruits/pineapple.png", category: "fruits", stock: 4 },
+  { id: 19, name: "שזיף", price: 10, unit: "ק״ג", image: "image/fruits/plum.png", category: "fruits", stock: 12 },
+  { id: 20, name: "רימון", price: 12, unit: "ק״ג", image: "image/fruits/Pomegranate.png", category: "fruits", stock: 7 }
+
+];
+
+
---
+++ JS/shop.js
@@ -0,0 +1,96 @@
+
+
+
+/*פונק' להדפסת הכרטיסים */
+function renderProducts(productsToShow) {
+    const container = document.getElementById("productsContainer");
+    container.innerHTML = "";
+    productsToShow.forEach(product => {
+        container.innerHTML += `
+
+  
+                    <div class="col-auto">
+                        <div class="card shadow-sm" style="width:208px; height:301px;">
+
+                            <!-- תמונה -->
+                            <img src="${product.image}" class="card-img-top" alt="רימון"
+                                style="height:160px; object-fit:contain; padding:15px;">
+
+                            <div class="card-body d-flex flex-column justify-content-between text-center">
+
+                                <!-- שם -->
+                                <div>
+                                    <h6 class="fw-bold mb-1">${product.name}</h6>
+                                    <div class="text-success fw-semibold small">
+                                        ₪${product.price} לק״ג
+                                    </div>
+                                </div>
+
+                                <!-- בחירת משקל -->
+                                <div class="d-flex justify-content-center align-items-center gap-2">
+
+                                    <button class="btn btn-outline-success btn-sm rounded-circle">
+                                        <i class="bi bi-dash"></i>
+                                    </button>
+
+                                    <div class="border rounded-pill px-3 py-1 small fw-semibold">
+                                        הוסף לסל
+                                    </div>
+
+                                    <button class="btn btn-success btn-sm rounded-circle">
+                                        <i class="bi bi-plus"></i>
+                                    </button>
+
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+`;
+    });
+
+    
+}
+
+function filterproducts(category)
+{
+    if(category==="all")
+        renderProducts(products);
+    else{
+    const filtered = products.filter(product => product.category === category);
+ renderProducts(filtered);
+    }
+
+  
+}
+
+/*קריאה לפונק' להדפסה*/
+document.addEventListener("DOMContentLoaded", function () {
+  renderProducts(products);
+});
+
+/*הדגשת כפתורי הנאב*/
+function setNavBtn(active){
+const buttons = document.querySelectorAll(".category-btn");
+
+buttons.forEach(btn => {btn.classList.remove("active","fw-bold","text-sucsses")})
+
+active.classList.add("active","fw-bold","text-success")
+}
+
+
+
+/*פונקצית חיפוש */
+const input=document.getElementById("searchInput")
+input.addEventListener("input",function (){
+const value = this.value.toLowerCase();
+
+if(value==="")
+{
+    renderProducts(products)
+}
+else{
+    const arrSearch = products.filter(product=> product.name.toLowerCase().includes(value))
+    renderProducts(arrSearch)
+}
+})
